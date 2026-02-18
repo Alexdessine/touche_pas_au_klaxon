6b8802fb35424e1469859a8c5880727f82ae72f9
Alexdessine <alexandre@bourlier.email>
2026-02-18 13:34:03 +0100

    suppression du dossier node_module du repository et mis à jour .gitignore

--- views/home/index.php
+++ views/home/index.php
@@ -1,2 +1,12 @@
-<?php
-echo 'Bienvenue sur la page d\'accueil !';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Bienvenue sur la page d'accueil !</h1>
+</body>
+</html>
